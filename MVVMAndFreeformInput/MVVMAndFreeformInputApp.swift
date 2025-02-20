ed17e47d588f47a2b9c255eea9f2f71f77b96665
Thomas Noone <tnoone@lcs.on.ca>
2025-02-20 09:42:10 -0500

    Initial Commit

---
+++ MVVMAndFreeformInput/MVVMAndFreeformInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMAndFreeformInputApp.swift
+//  MVVMAndFreeformInput
+//
+//  Created by Thomas Noone on 2025-02-20.
+//
+
+import SwiftUI
+
+@main
+struct MVVMAndFreeformInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
