95b77e6216939ae560a61f0bb9edd9c0aef72fe1
Thomas Noone <tnoone@lcs.on.ca>
2025-02-21 11:08:27 -0500

    Added the View Model

---
+++ MVVMAndFreeformInput/ViewModels/PowerViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  PowerViewModel.swift
+//  MVVMAndFreeformInput
+//
+//  Created by Thomas Noone on 2025-02-21.
+//
+
+import SwiftUI
+
+struct PowerViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PowerViewModel()
+}
