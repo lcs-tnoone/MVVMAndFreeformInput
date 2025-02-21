03795c334276c7ec6b267c0bde1fd9b58ac7ffd2
Thomas Noone <tnoone@lcs.on.ca>
2025-02-21 11:13:40 -0500

    Added a view layer to allow for the user to input and see the output.

--- MVVMAndFreeformInput/Model/Power.swift
+++ MVVMAndFreeformInput/Model/Power.swift
@@ -1,18 +1,49 @@
-//
-//  Power.swift
-//  MVVMAndFreeformInput
-//
-//  Created by Thomas Noone on 2025-02-21.
-//
+import Foundation
 
-import SwiftUI
-
-struct Power: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+// MODEL
+struct Power {
+    
+    // MARK: Stored properties
+    
+    // The base of the power can hold any numeric value
+    var base: Double
+    
+    // The exponent of a power must be an integer
+    var exponent: Int
+    
+    // MARK: Computed properties
+    
+    // A power is simply a shorter way of expressing
+    // repeated multiplication.
+    //
+    // e.g.: 3^2
+    //       = 3 * 3
+    //       = 9
+    //
+    // The base, 3, mutiplied by itself twice, resulting in 9
+    //
+    // This could also be expressed as:
+    //
+    // e.g.: 3^2
+    //       = 1 * 3 * 3
+    //       = 9
+    //
+    // We will implement code that mimics this second example.
+    var result: Double {
+        
+        // Start by making the solution equal to 1
+        var solution = 1.0
+        
+        // Repeatedly multiply the base by itself
+        // as many more times as needed
+        for _ in 1...exponent {
+            solution *= base
+        }
+        
+        // Return the solution
+        return solution
+        
     }
+    
 }
 
-#Preview {
-    Power()
-}
--- MVVMAndFreeformInput/ViewModels/PowerViewModel.swift
+++ MVVMAndFreeformInput/ViewModels/PowerViewModel.swift
@@ -5,14 +5,62 @@
 //  Created by Thomas Noone on 2025-02-21.
 //
 
-import SwiftUI
-
-struct PowerViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+import Foundation
+@Observable
+// VIEW MODEL
+class PowerViewModel {
+    
+    // MARK: Stored properties
+    
+    // Holds whatever the user has typed in the text fields
+    var providedBase: String
+    var providedExponent: String
+    
+    // Holds an appropriate error message, if there was a
+    // problem with input provided by the user
+    var recoverySuggestion: String = ""
+    
+    // MARK: Computed properties
+    // Holds the evaluated power, when the input provided is valid
+    var power: Power? {
+        
+        // First check that the string in providedBase can
+        // be converted into a number, then check that the
+        // value is more than 0
+        guard let base = Double(providedBase), base > 0 else {
+            recoverySuggestion = "Please provide a positive value for the base of the power."
+            
+            return nil
+        }
+        
+        // Now check that the string in providedExponent can be
+        // converted into an integer, and that the value is
+        // more than or equal to 1
+        guard let exponent = Int(providedExponent), exponent >= 1 else {
+            recoverySuggestion = "Please provide an integer value of 1 or greater for the exponent."
+            
+            return nil
+        }
+        
+        // Now that we know the base and exponent have valid values, return the evaluated power
+        recoverySuggestion = "" // No error message
+        return Power(base: base, exponent: exponent)
+        
     }
+    
+    // MARK: Initializer(s)
+    
+    init(
+        providedBase: String = "",
+        providedExponent: String = "",
+        recoverySuggestion: String = ""
+    ) {
+        self.providedBase = providedBase
+        self.providedExponent = providedExponent
+        self.recoverySuggestion = recoverySuggestion
+    }
+    
 }
 
-#Preview {
-    PowerViewModel()
-}
+// Create an instance of the View Model
+var viewModel = PowerViewModel()
