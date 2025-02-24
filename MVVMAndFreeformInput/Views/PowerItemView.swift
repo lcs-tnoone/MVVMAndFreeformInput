570890e1e2b64366ed16a20654ed2277571138f3
Thomas Noone <tnoone@lcs.on.ca>
2025-02-24 15:12:49 -0500

    Made it possible to see the array of previously calculated results in a scrollable list.

---
+++ MVVMAndFreeformInput/Views/PowerItemView.swift
@@ -0,0 +1,18 @@
+//
+//  PowerItemView.swift
+//  MVVMAndFreeformInput
+//
+//  Created by Thomas Noone on 2025-02-24.
+//
+
+import SwiftUI
+
+struct PowerItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PowerItemView()
+}
