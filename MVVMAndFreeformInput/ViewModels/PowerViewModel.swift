8c098af41376755d35fe3dc50df6ffabdbfe8c6f
Thomas Noone <tnoone@lcs.on.ca>
2025-02-24 14:40:29 -0500

    Made view model changes to support saving a history of previous calculations.

--- MVVMAndFreeformInput/ViewModels/PowerViewModel.swift
+++ MVVMAndFreeformInput/ViewModels/PowerViewModel.swift
@@ -12,6 +12,10 @@ class PowerViewModel {
     
     // MARK: Stored properties
     
+    // Holds the list of previously computed and evaluated powers
+    var resultHistory: [Power] = []
+
+    
     // Holds whatever the user has typed in the text fields
     var providedBase: String
     var providedExponent: String
@@ -63,6 +67,23 @@ class PowerViewModel {
         self.recoverySuggestion = recoverySuggestion
     }
     
+    // MARK: Function(s)
+    func saveResult() {
+        
+        // When there is a valid power based on user input...
+        if let power = self.power {
+            
+            // ... save that evaluated power at the top of the history of
+            // results
+            //
+            // NOTE: By inserting the newly evaluated power at the top of
+            //       the array, we ensure the user sees
+            //       the most recent result first.
+            self.resultHistory.insert(power, at: 0)
+        }
+        
+    }
+
 }
 
 // Create an instance of the View Model
